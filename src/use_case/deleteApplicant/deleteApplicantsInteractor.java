8f707828020c7186a02d8307bc288ea6a0c7845a
shahbazsingh <shahbaz.nanda@mail.utoronto.ca>
2023-11-15 15:54:10 -0500

    "Creating deleteApplicantsInteractor"

---
+++ src/use_case/deleteApplicant/deleteApplicantsInteractor.java
@@ -0,0 +1,20 @@
+package use_case.deleteApplicant;
+
+public class deleteApplicantsInteractor implements deleteApplicantsInputBoundary{
+
+    final deleteApplicantsDataAccessInterface applicantsDataAccessObject;
+
+    final deleteApplicantsOutputBoundary applicantsPresenter;
+
+    public deleteApplicantsInteractor(deleteApplicantsDataAccessInterface applicantsDataAccessObject, deleteApplicantsOutputBoundary applicantsPresenter) {
+        this.applicantsDataAccessObject = applicantsDataAccessObject;
+        this.applicantsPresenter = applicantsPresenter;
+    }
+
+    @Override
+    public void execute(deleteApplicantsInputData inputData) {
+        applicantsDataAccessObject.deleteApplicants(inputData.getApplicantIDs());
+        deleteApplicantsOutputData outputData = new deleteApplicantsOutputData(applicantsDataAccessObject.getApplicants());
+        applicantsPresenter.prepareSuccessView(outputData);
+    }
+}
