2799380425bf9b3ae5cc89fd81a160f42f150771
shahbazsingh <shahbaz.nanda@mail.utoronto.ca>
2023-11-15 15:54:30 -0500

    "Creating deleteApplicantsOutputBoundary.java"

---
+++ src/use_case/deleteApplicant/deleteApplicantsOutputBoundary.java
@@ -0,0 +1,8 @@
+package use_case.deleteApplicant;
+
+import use_case.resumeParsing.ResumeParsingOutputData;
+
+public interface deleteApplicantsOutputBoundary {
+    void prepareSuccessView(deleteApplicantsOutputData outputData);
+
+}
