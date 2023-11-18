6f1bf585c81949824d60b0b2c0e2602e9d6bf92f
taha <tahausman2303@gmail.com>
2023-11-18 15:15:00 -0500

    .

--- src/use_case/showHireApplicantPage/ShowHireApplicantPageOutputBoundary.java
+++ src/use_case/showHireApplicantPage/ShowHireApplicantPageOutputBoundary.java
@@ -3,5 +3,5 @@ package use_case.showHireApplicantPage;
 import entity.Applicant;
 
 public interface ShowHireApplicantPageOutputBoundary {
-    public void prepareSuccessView(Applicant applicant);
+    void prepareSuccessView(Applicant applicant);
 }
