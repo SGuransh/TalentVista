e16dc910da254ea39c31be3e2f0a52ac06a02309
shahbazsingh <shahbaz.nanda@mail.utoronto.ca>
2023-11-18 11:14:56 -0500

    Completing Show Applicants Use Case

--- src/app/HrDashboardUseCaseFactory.java
+++ src/app/HrDashboardUseCaseFactory.java
@@ -5,9 +5,11 @@ import data_access.InMemoryEmployeeAccessObject;
 import interface_adapter.HrDashboard.HrDashboardViewModel;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.showApplicants.ShowApplicantsController;
+import interface_adapter.showApplicants.ShowApplicantsPresenter;
 import interface_adapter.showApplicants.ShowApplicantsViewModel;
 import interface_adapter.showEmployees.ShowEmployeesController;
 import interface_adapter.showEmployees.ShowEmployeesViewModel;
+import use_case.showApplicants.*;
 import view.HrDashboardView;
 
 import javax.swing.*;
@@ -16,7 +18,7 @@ import java.io.IOException;
 public class HrDashboardUseCaseFactory {
     private HrDashboardUseCaseFactory() {};
 
-    public static HrDashboardView create(ViewManagerModel viewManagerModel, HrDashboardViewModel hrDashboardViewModel, ShowEmployeesViewModel showEmployeesViewModel, ShowApplicantsViewModel showApplicantsViewModel, InMemoryApplicantAccessObject applicantsDataAccessObject, InMemoryEmployeeAccessObject employeeDataAccessObject){
+    public static HrDashboardView create(ViewManagerModel viewManagerModel, HrDashboardViewModel hrDashboardViewModel, ShowEmployeesViewModel showEmployeesViewModel, ShowApplicantsViewModel showApplicantsViewModel, ShowApplicantsDataAccessInterface applicantsDataAccessObject, InMemoryEmployeeAccessObject employeeDataAccessObject){
         try {
             ShowApplicantsController showApplicantsController = createShowApplicantsUseCase(viewManagerModel, showApplicantsViewModel, applicantsDataAccessObject);
             ShowEmployeesController showEmployeesController = createShowEmployeesUseCase(viewManagerModel, showEmployeesViewModel, employeeDataAccessObject);
@@ -29,8 +31,10 @@ public class HrDashboardUseCaseFactory {
         return null;
     }
 
-    private static ShowApplicantsController createShowApplicantsUseCase(ViewManagerModel viewManagerModel, ShowApplicantsViewModel showApplicantsViewModel, InMemoryApplicantAccessObject applicantsDataAccessObject){
-
+    private static ShowApplicantsController createShowApplicantsUseCase(ViewManagerModel viewManagerModel, ShowApplicantsViewModel showApplicantsViewModel, ShowApplicantsDataAccessInterface applicantsDataAccessObject){
+        ShowApplicantsOutputBoundary showApplicantsPresenter = new ShowApplicantsPresenter(showApplicantsViewModel, viewManagerModel);
+        ShowApplicantsInputBoundary showApplicantsInteractor = new ShowApplicantsInteractor(showApplicantsPresenter, applicantsDataAccessObject);
+        return new ShowApplicantsController(showApplicantsInteractor);
 
     }
     private static ShowEmployeesController createShowEmployeesUseCase(ViewManagerModel viewManagerModel, ShowEmployeesViewModel showEmployeesViewModel, InMemoryEmployeeAccessObject employeeDataAccessObject){
---
+++ src/interface_adapter/showApplicants/ShowApplicantsPresenter.java
@@ -0,0 +1,20 @@
+package interface_adapter.showApplicants;
+
+import interface_adapter.ViewManagerModel;
+import use_case.showApplicants.ShowApplicantsOutputBoundary;
+import use_case.showApplicants.ShowApplicantsOutputData;
+
+public class ShowApplicantsPresenter implements ShowApplicantsOutputBoundary {
+    private final ShowApplicantsViewModel showApplicantsViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public ShowApplicantsPresenter(ShowApplicantsViewModel showApplicantsViewModel, ViewManagerModel viewManagerModel) {
+        this.showApplicantsViewModel = showApplicantsViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    @Override
+    public void prepareSuccessView(ShowApplicantsOutputData outputData) {
+
+    }
+}
