24d883406b21ac2899a1c636de1541aecc0991eb
shahbazsingh <shahbaz.nanda@mail.utoronto.ca>
2023-11-17 19:22:10 -0500

    "New UseCase, SHowCreateEmployeePAge"

---
+++ src/use_case/deleteApplicants/deleteApplicantsDataAccessInterface.java
@@ -0,0 +1,13 @@
+package use_case.deleteApplicants;
+
+import entity.Applicant;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public interface deleteApplicantsDataAccessInterface {
+    void deleteApplicants(ArrayList<String> applicantIDs);
+    Map<String, Applicant> getApplicants();
+
+    String getPresentableApplicants();
+}
