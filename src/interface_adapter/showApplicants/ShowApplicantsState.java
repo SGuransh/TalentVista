d313bd3023e59c9a848b43911d85945f38710b77
shahbazsingh <shahbaz.nanda@mail.utoronto.ca>
2023-11-18 17:42:12 -0500

    Creating Controllers for Filter Case and Delete Case

--- src/interface_adapter/showApplicants/ShowApplicantsState.java
+++ src/interface_adapter/showApplicants/ShowApplicantsState.java
@@ -1,4 +1,79 @@
 package interface_adapter.showApplicants;
 
 public class ShowApplicantsState {
+    private String applicantsToDelete = "";
+    private String applicantToHire = "";
+    private String dateToFilter = "";
+    private String skillToFilter = null;
+    private String filesToUpload = "";
+
+
+    public SignupState(SignupState copy) {
+        username = copy.username;
+        usernameError = copy.usernameError;
+        password = copy.password;
+        passwordError = copy.passwordError;
+        repeatPassword = copy.repeatPassword;
+        repeatPasswordError = copy.repeatPasswordError;
+    }
+
+    // Because of the previous copy constructor, the default constructor must be explicit.
+    public SignupState() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUsernameError() {
+        return usernameError;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPasswordError() {
+        return passwordError;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public String getRepeatPasswordError() {
+        return repeatPasswordError;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUsernameError(String usernameError) {
+        this.usernameError = usernameError;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPasswordError(String passwordError) {
+        this.passwordError = passwordError;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
+
+    public void setRepeatPasswordError(String repeatPasswordError) {
+        this.repeatPasswordError = repeatPasswordError;
+    }
+
+    public void setDeletedUsers(String deletedUsers){
+        this.deletedUsers = deletedUsers;
+    }
+
+    public String getDeletedUsers(){
+        return this.deletedUsers;
+    }
 }
