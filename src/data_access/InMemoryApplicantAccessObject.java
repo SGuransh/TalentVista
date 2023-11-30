e63202931c1d229c2426ac773c1a6c4d8435be14
shahbazsingh <shahbaz.nanda@mail.utoronto.ca>
2023-11-30 18:51:10 -0500

    Creating Data Persistence for Applicants

--- src/data_access/InMemoryApplicantAccessObject.java
+++ src/data_access/InMemoryApplicantAccessObject.java
@@ -7,7 +7,9 @@ import use_case.resumeParsing.ResumeParsingDataAccessInterface;
 import use_case.showApplicants.ShowApplicantsDataAccessInterface;
 import use_case.showHireApplicantPage.ShowHireApplicantPageDataAccessInterface;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,12 +41,12 @@ public class InMemoryApplicantAccessObject implements ResumeParsingDataAccessInt
 
     @Override
     public void deleteApplicants(ArrayList<String> applicantIDs) {
-        for (String applicantID: applicantIDs){
+        for (String applicantID : applicantIDs) {
             applicants.remove(applicantID);
         }
     }
 
-    public Map<String, Applicant> getApplicants(){
+    public Map<String, Applicant> getApplicants() {
         return applicants;
     }
 
@@ -53,26 +55,100 @@ public class InMemoryApplicantAccessObject implements ResumeParsingDataAccessInt
         return id;
     }
 
-    public String getPresentableApplicants(){
+    public String getPresentableApplicants() {
         StringBuilder presentableApplicants = new StringBuilder();
-        for (String applicantID: applicants.keySet()){
+        for (String applicantID : applicants.keySet()) {
             Map<String, Applicant> applicantMap = applicants;
             Applicant applicant = applicantMap.get(applicantID);
             String name = applicant.getName();
             String ID = applicant.getId();
             ArrayList<String> skillsArray = applicant.getSkills();
             StringBuilder skillsString = new StringBuilder();
-            for (String skill: skillsArray){
+            for (String skill : skillsArray) {
                 skillsString.append(skill).append(", ");
             }
             HashMap<String, String> contactInfo = applicant.getContactInfo();
             String email = contactInfo.get("email");
             String phoneNum = contactInfo.get("phone");
-            presentableApplicants.append("ID: " + ID+ " NAME: " + name + "SKILLS: " + skillsString + "EMAIL: " + email + "PHONE: " + phoneNum);
+            presentableApplicants.append("ID: " + ID + " NAME: " + name + "SKILLS: " + skillsString + "EMAIL: " + email + "PHONE: " + phoneNum);
             presentableApplicants.append("\n\n");
-    }
+        }
         return presentableApplicants.toString();
     }
+
+    public void ReadCsvToInMemory() {
+        String csvFile = "src/data_access/Applicants.csv";
+        String line;
+        String csvSplitBy = ","; // CSV files typically use commas as separators
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(csvSplitBy);
+
+                String id = data[0];
+
+                String name = data[1];
+                String skillsString = data[2];
+                String uploadDate = data[3];
+                String personal_urls_String = data[4];
+                String contactInfo_String = data[5];
+                String position = data[6];
+
+                skillsString = skillsString.replace("\u0016", ",").trim();
+                String[] skillsArray = skillsString.substring(1, skillsString.length()-1).split(", ");
+
+                personal_urls_String = personal_urls_String.replace("\u0016", ",").trim();
+                String[] urls_array = personal_urls_String.substring(1, personal_urls_String.length()-1).split(",");
+                // Convert the array to an ArrayList
+                ArrayList<String> skillsArrayList = new ArrayList<>(Arrays.asList(skillsArray));
+                //Converting URLS to HashMap
+                ArrayList<String> UrlsArrayList = new ArrayList<>(Arrays.asList(urls_array));
+                //Converting URLS to HashMap
+
+                //Converting Contacts to HashMap
+                String[] contacts_String_noBracket = contactInfo_String.substring(1, contactInfo_String.length() - 1).split(", ");
+
+                HashMap<String, String> contacts_map = new HashMap<>();
+
+                for (String pair : contacts_String_noBracket) {
+                    String[] entry = pair.split("=");
+                    contacts_map.put(entry[0], entry[1]);
+                }
+                //Converting Contacts to HashMap
+
+                Applicant applicant = new Applicant(id, name, skillsArrayList, uploadDate, contacts_map, UrlsArrayList, position);
+                this.addApplicant(applicant);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveToCsv(){
+        String csvFilePath = "src/data_access/Applicants.csv";
+
+        try {
+            // Open the CSV file in append mode (this will not truncate the file)
+            PrintWriter writer = new PrintWriter(new FileWriter(csvFilePath));
+
+            for (String key : this.applicants.keySet()) {
+                String lineToWrite = "";
+                Applicant applicant = this.applicants.get(key);
+                lineToWrite += applicant.getId() + "," + applicant.getName() + "," + applicant.getSkills().toString().replace(",", "\u0016") + "," + applicant.getUploadDate() + "," + applicant.personal_urls().toString().replace(",", "\u0016") + "," + applicant.getContactInfo().toString() + "," + applicant.getPosition();
+                writer.println(lineToWrite);
+            }
+
+            // Close the writer to ensure changes are flushed and the file is released
+            writer.close();
+
+            System.out.println("CSV lines written successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
