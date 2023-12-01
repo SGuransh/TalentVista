0115658ea16cd8d4c5948700b77cd24db4fc21c2
shahbazsingh <shahbaz.nanda@mail.utoronto.ca>
2023-11-30 19:36:43 -0500

    Creating Data Persistence for Employees

--- src/data_access/InMemoryEmployeeAccessObject.java
+++ src/data_access/InMemoryEmployeeAccessObject.java
@@ -5,6 +5,7 @@ import entity.Employee;
 import use_case.HireApplicantButton.HireApplicantDataAccessInterface;
 import use_case.showEmployees.ShowEmployeesDataAccessInterface;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -44,6 +45,51 @@ public class InMemoryEmployeeAccessObject implements HireApplicantDataAccessInte
             presentableEmployees.append("\n\n");
         }
         return presentableEmployees.toString();
+    }
+
+    public void ReadCsvToInMemory(){
+        String csvFile = "src/data_access/Employees.csv";
+        String line;
+        String csvSplitBy = ","; // CSV files typically use commas as separators
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(csvSplitBy);
+                String name = data[0];
+                String password = data[1];
+                Double salary = Double.parseDouble(data[2]);
+                String position = data[3];
+                String email = data[4];
+
+                Employee employee = new Employee(name, password, null, salary, position, email);
+                this.addEmployee(employee);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveToCsv(){
+        String csvFilePath = "src/data_access/Employees.csv";
+
+        try {
+            // Open the CSV file in append mode (this will not truncate the file)
+            PrintWriter writer = new PrintWriter(new FileWriter(csvFilePath));
+
+            for (String key : this.employees.keySet()) {
+                String lineToWrite = "";
+                Employee employee = this.employees.get(key);
+                lineToWrite += employee.getName() + "," + employee.getPassword() + "," + employee.getSalary().toString() + "," + employee.getPosition()+ "," + employee.getEmail();
+                writer.println(lineToWrite);
+            }
+
+
+            // Close the writer to ensure changes are flushed and the file is released
+            writer.close();
+            System.out.println("CSV lines written successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
